f1c360d443d32338b9f8252b06492217f31fd810
jkp53 <80708001+jkp53@users.noreply.github.com>
2022-03-30 18:02:24 -0400

    added pytest stuff

--- app/utils.py
+++ app/utils.py
@@ -6,6 +6,8 @@ def to_usd(my_price):
     What this function will return.
 
     Invoke like this: to_usd(9.9999)
+
+    Example return value "9.99"
     """
     return '${:,.2f}'.format(my_price)
 
